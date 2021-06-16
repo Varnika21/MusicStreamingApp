06b07bb07e60555db755b211ffae1923941132af
Varnika21 <samudralalaxmivarnika@gmail.com>
2021-06-16 20:35:16 +0530

    Final Commit

--- Musicstreaming/views.py
+++ Musicstreaming/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from MusicBeats.models import Song,Watchlater
+from MusicBeats.models import Song,Watchlater,Favourites
 from django.db.models import Case,When
 
 def index(request):
@@ -16,5 +16,17 @@ def index(request):
         watch = reversed(watch)
     else:
         watch = Song.objects.all()[0:4]
+
+    if request.user.is_authenticated:
+        f1 = Favourites.objects.filter(user=request.user)
+        ids = []
+        for i in f1:
+            ids.append(i.fav_song_id)
+
+        preserved = Case(*[When(pk=pk, then=pos) for pos,pk in enumerate(ids)])
+        fav = Song.objects.filter(song_id__in=ids).order_by(preserved)
+        fav = reversed(fav)
+    else:
+        fav = Song.objects.all()[0:4]
     
-    return render(request,'index.htm',{'song': song, 'watch': watch})
+    return render(request,'index.htm',{'song': song, 'watch': watch, 'fav':fav })
